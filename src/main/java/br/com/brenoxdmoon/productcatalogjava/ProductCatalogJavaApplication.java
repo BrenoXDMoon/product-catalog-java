ec7a0b10497cc9ab09e0acff56fc576edf98dbc8
Breno Gabriel <brenogabriel19@gmail.com>
2024-05-08 15:05:35 -0300

    Initial commit

---
+++ src/main/java/br/com/brenoxdmoon/productcatalogjava/ProductCatalogJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.brenoxdmoon.productcatalogjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brenoxdmoon/productcatalogjava/ProductCatalogJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brenoxdmoon.productcatalogjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
